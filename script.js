c80023d5159b75b55286229a1a08ebe5daa2c399
Bar Yaakov <baryaakov5050@gmail.com>
2025-06-17 17:49:54 -0400

    Finished my 6

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function mySome(array, condition)
             return true;
         }
     }
+
     return false;
 }
 
@@ -37,22 +38,46 @@ function myIncludes(array, target)
             return true;
         }
     }
+
     return false;
 }
 
 //.push
 
+function myPush(array, value)
+{
+    array[array.length] = value;
 
+    return array.length;
+}
 
+//.keys
 
+function myKeys(object)
+{
+    let array = [];
 
+    for (let key in object)
+    {
+        array[array.length] = key;
+    }
 
+    return array;
+}
 
+//.values
 
+function myValues(object)
+{
+    let array = [];
 
+    for (let value in object)
+    {
+        array[array.length] = object[value];
+    }
 
-
-
+    return array;
+}
 
 
 
@@ -97,5 +122,41 @@ console.log(mySome(arr2, condition));
 arr3 = ["dog", "cat", "bunny"];
 
 console.log(myIncludes(arr3, "bunny"));
+
+//******************************************************************************
+
+//testing myPush
+
+arr4 = [1,2,3,4,5];
+
+myPush(arr4, 6);
+
+console.log(arr4);
+
+//******************************************************************************
+
+//testing myKeys
+
+var object =
+{
+    Name: "Bar",
+    Age: "25",
+    Favorite_Show: "Attack on Titan"
+};
+
+console.log(myKeys(object));
+
+//******************************************************************************
+
+//testing myValues
+
+var object =
+{
+    Name: "Bar",
+    Age: "25",
+    Favorite_Show: "Attack on Titan"
+};
+
+console.log(myValues(object));
 
 //******************************************************************************
