ba7aa6a31e4b5c22107ec87b732ecb0d254e69fe
Asmaa-ait <asmaa.ait01@gmail.com>
2025-06-17 15:22:19 -0400

    function myMap

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ myEach(numbers, function(number) {
   console.log(number); // 1 2 3 4 5
 });
 
+//-----------------------------------------------------------------------------------------------------
+
+function myMap(array, callback) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(callback(array[i], i, array));
+  }
+  return result;
+}
+let num = [1, 2, 3, 4, 5];
+
+let doubledNumbers = myMap(numbers, function(number) {
+  return number * 2;
+});
+console.log(doubledNumbers); // [2, 4, 6, 8, 10]
