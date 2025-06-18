6fe54e38e72ab4bbab3c48ef944dc6c9f024abe1
Bar Yaakov <baryaakov5050@gmail.com>
2025-06-17 20:16:52 -0400

    Finished all 12

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//.ForEach
+
+function myForEach(array, action)
+{
+    for (let i = 0; i < array.length; i++)
+    {
+        action(array[i]);
+    }
+}
+
 //.map
 
 function myMap(array, action)
@@ -7,6 +17,23 @@ function myMap(array, action)
     for (let i = 0; i < array.length; i++)
     {
         newArray[i] = action(array[i]);
+    }
+
+    return newArray;
+}
+
+//.filter
+
+function myFilter(array, condition)
+{
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++)
+    {
+        if (condition(array[i]))
+        {
+            newArray[newArray.length] = array[i];
+        }
     }
 
     return newArray;
@@ -27,6 +54,46 @@ function mySome(array, condition)
     return false;
 }
 
+//.every
+
+function myEvery(array, condition)
+{
+    for (let i = 0; i < array.length; i++)
+    {
+        if (!condition(array[i]))
+        {
+            return false;
+        }
+    }
+    return true
+}
+
+//.reduce
+
+function myReduce(array, reducer, initialValue)
+{
+    let accumulator;
+    let startIndex;
+
+    if (initialValue !== undefined)
+    {
+        accumulator = initialValue;
+        startIndex = 0;
+    }   
+    else if (initialValue === undefined)
+    {
+        accumulator = array[0];
+        startIndex = 1;
+    }
+
+    for (let i = startIndex; i < array.length; i++)
+    {
+        accumulator = reducer(accumulator, array[i])
+    }
+
+    return accumulator;
+}
+
 //.includes
 
 function myIncludes(array, target)
@@ -42,6 +109,21 @@ function myIncludes(array, target)
     return false;
 }
 
+//.indexOf
+
+function myIndexOf(array, target)
+{
+    for (let i = 0; i < array.length; i++)
+    {
+        if (array[i] === target)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
 //.push
 
 function myPush(array, value)
@@ -49,6 +131,21 @@ function myPush(array, value)
     array[array.length] = value;
 
     return array.length;
+}
+
+//.lastIndexOf
+
+function myLastIndexOf(array, target)
+{
+    for (let i = array.length - 1; i >=0; i--)
+    {
+        if (array[i] === target)
+        {
+            return i;
+        }
+    }
+
+    return -1;
 }
 
 //.keys
@@ -79,33 +176,59 @@ function myValues(object)
     return array;
 }
 
+console.log("***********************testing***********************");
 
+//testing myForEach
 
+console.log("myForEach prints out each element of the array:")
 
+let myForEachArray = [1,2,3,4,5];
 
+myForEach(myForEachArray, console.log);
 
-
-//************************************testing***********************************
+console.log("*****************************************************");
 
 //testing myMap
 
-arr = [1,2,3,4,5];
+console.log("myMap multiplies each element of the array by 2 and returns a new array with the results")
 
-function func(x)
+let myMapArray = [1,2,3,4,5];
+
+function myMapAction(x)
 {
     x = x * 2;
     return x;
 }
 
-console.log(myMap(arr, func));
+console.log(myMap(myMapArray, myMapAction));
 
-//******************************************************************************
+console.log("*****************************************************");
+
+//testing myFilter
+
+console.log("myFilter checks each element of the array if it's smaller or equals to 3 and returns a new array with those numbers")
+
+let myFilterArray = [1,2,3,4,5];
+
+function myFilterCondition(x)
+{
+    if (x <= 3)
+    {
+        return true
+    }
+}
+
+console.log(myFilter(myFilterArray, myFilterCondition));
+
+console.log("*****************************************************");
 
 //testing mySome
 
-arr2 = [1,2,3,4,5];
+console.log("mySome runs a condition over each element of the array until it finds 1 that meets the condition, if it does, it returns true")
 
-function condition(x)
+let mySomeArray = [1,2,3,4,5];
+
+function mySomeCondition(x)
 {
     if (x > 3)
     {
@@ -113,50 +236,111 @@ function condition(x)
     }
 }
 
-console.log(mySome(arr2, condition));
+console.log(mySome(mySomeArray, mySomeCondition));
 
-//******************************************************************************
+console.log("*****************************************************");
+
+//testing myEvery
+
+console.log("myEvery runs a condition over all elements of the array and checks if all of them meet the condition, if all of them do, it returns true")
+
+let myEveryArray = [1,2,3,4,5];
+
+function myEveryCondition(x)
+{
+    if (x < 6)
+    {
+        return true
+    }
+}
+
+console.log(myEvery(myEveryArray, myEveryCondition));
+
+console.log("*****************************************************");
+
+//testing myReduce
+
+console.log("myReduce goes through each element of the array and returns a combined single value using a provided function")
+
+let myReduceArray = [1,2,3,4,5];
+
+function myReduceAddNumbers(total, arrayIndexValue)
+{
+    return total + arrayIndexValue;
+}
+
+console.log(myReduce(myReduceArray, myReduceAddNumbers ,0));
+
+console.log("*****************************************************");
 
 //testing myIncludes
 
-arr3 = ["dog", "cat", "bunny"];
+console.log("myIncludes looks through each element of the array for a specific value, if the array has that value, it returns true")
 
-console.log(myIncludes(arr3, "bunny"));
+let myIncludeArray = ["dog", "cat", "bunny"];
 
-//******************************************************************************
+console.log(myIncludes(myIncludeArray, "bunny"));
+
+console.log("*****************************************************");
+
+//testing myIndexOf
+
+console.log("myIndexOf looks through each element of the array for a specific value, if the array has that value, it returns the index in which the value exists at")
+
+let myIndexOfArray = ["dog", "cat", "bunny"];
+
+console.log(myIndexOf(myIndexOfArray, "bunny"));
+
+console.log("*****************************************************");
 
 //testing myPush
 
-arr4 = [1,2,3,4,5];
+console.log("myPush adds an element to the end of the array and returns the new length of the array");
 
-myPush(arr4, 6);
+let myPushArray = [1,2,3,4,5];
 
-console.log(arr4);
+myPush(myPushArray, 6);
 
-//******************************************************************************
+console.log(myPushArray);
+
+console.log("*****************************************************");
+
+//testing myLastIndexOf
+
+console.log("myLastIndexOf looks through each element of the array starting from the last one for a specific value, if the array has that value, it returns the last index in which the value exists at");
+
+let myLastIndexOfArray = ["dog", "cat", "bunny", "axolotl", "guinea pig", "bunny", "hamster"];
+
+console.log(myLastIndexOf(myLastIndexOfArray, "bunny"));
+
+console.log("*****************************************************");
 
 //testing myKeys
 
-var object =
+console.log("myKeys accepts an object and returns an array filled with the keys of that object");
+
+let myKeysObject =
 {
     Name: "Bar",
     Age: "25",
     Favorite_Show: "Attack on Titan"
 };
 
-console.log(myKeys(object));
+console.log(myKeys(myKeysObject));
 
-//******************************************************************************
+console.log("*****************************************************");
 
 //testing myValues
 
-var object =
+console.log("myValues accepts an object and returns an array filled with the values of that object");
+
+let myValuesObject =
 {
     Name: "Bar",
     Age: "25",
     Favorite_Show: "Attack on Titan"
 };
 
-console.log(myValues(object));
+console.log(myValues(myValuesObject));
 
-//******************************************************************************
+console.log("*****************************************************");
